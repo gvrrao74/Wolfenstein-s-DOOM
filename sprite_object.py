b712f789caeb084ee620afecd3a165c7f4c64fef
gvrrao74 <chiranjeeviraodv.74@gmail.com>
2026-01-10 05:57:25 +0530

    Use resource_path for loading images in SpriteObject

--- sprite_object.py
+++ sprite_object.py
@@ -2,7 +2,7 @@ import pygame as pg
 from settings import *
 import os
 from collections import deque
-
+from resource_manager import resource_path
 
 class SpriteObject:
     def __init__(self, game, path='resources/sprites/static_sprites/candlebra.png',
@@ -10,7 +10,7 @@ class SpriteObject:
         self.game = game
         self.player = game.player
         self.x, self.y = pos
-        self.image = pg.image.load(path).convert_alpha()
+        self.image = pg.image.load(resource_path(path)).convert_alpha()
         self.IMAGE_WIDTH = self.image.get_width()
         self.IMAGE_HALF_WIDTH = self.image.get_width() // 2
         self.IMAGE_RATIO = self.IMAGE_WIDTH / self.image.get_height()
@@ -82,8 +82,9 @@ class AnimatedSprite(SpriteObject):
 
     def get_images(self, path):
         images = deque()
-        for file_name in os.listdir(path):
-            if os.path.isfile(os.path.join(path, file_name)):
-                img = pg.image.load(path + '/' + file_name).convert_alpha()
+        resolved_path = resource_path(path) 
+        for file_name in os.listdir(resolved_path):
+            if os.path.isfile(os.path.join(resolved_path, file_name)):
+                img = pg.image.load(os.path.join(resolved_path, file_name)).convert_alpha()
                 images.append(img)
         return images
